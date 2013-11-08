02df00ea79f925d47c268d9866f9594262f9b056
hui.yang <hui.yang@dena.jp>
2013-11-08 18:14:30 +0800

    manifest version set to 1.4

--- src/com/eric/autowifi/BluetoothReceiver.java
+++ src/com/eric/autowifi/BluetoothReceiver.java
@@ -16,58 +16,42 @@ import android.widget.Toast;
 public class BluetoothReceiver extends BroadcastReceiver {
 	@SuppressLint("NewApi")
 	@Override
-	public void onReceive(final Context context, Intent intent)
-	{
+	public void onReceive(final Context context, Intent intent) {
 		final BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-		if (adapter != null)
-		{
+		if (adapter != null) {
 			String action = intent.getAction();
-			if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action))
-			{
+			if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
 				System.out.println("ACTION_ACL_CONNECTED");
-			}
-			else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action))
-			{
+			} else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
 				System.out.println("ACTION_ACL_DISCONNECTED");
-			}
-			else if (BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED
-					.equals(action))
-			{
+			} else if (BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED
+					.equals(action)) {
 				System.out.println("ACTION_ACL_DISCONNECT_REQUESTED");
-			}
-			else if (BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED
-					.equals(action) && Utils.getBluetoothA2dpToggle(context))
-			{
+			} else if (BluetoothA2dp.ACTION_CONNECTION_STATE_CHANGED
+					.equals(action) && Utils.getBluetoothA2dpToggle(context)) {
 				final BluetoothDevice device = intent
 						.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 				final String deviceName = device.getName();
 				ServiceListener listener = new ServiceListener() {
-					public void onServiceDisconnected(int profile)
-					{
+					public void onServiceDisconnected(int profile) {
 					}
 
 					public void onServiceConnected(int profile,
-							BluetoothProfile proxy)
-					{
-						if (profile == BluetoothProfile.HEADSET)
-						{
+							BluetoothProfile proxy) {
+						if (profile == BluetoothProfile.HEADSET) {
 							// _headset = (BluetoothHeadset) proxy; //
 							// _headset为BluetoothHeadset类型
-						}
-						else if (profile == BluetoothProfile.A2DP)
-						{
+						} else if (profile == BluetoothProfile.A2DP) {
 							BluetoothA2dp a2dp = (BluetoothA2dp) proxy; // _A2dp为BluetoothA2dp类型
 							int connectState = a2dp.getConnectionState(device);
-							switch (connectState)
-							{
+							switch (connectState) {
 							case BluetoothProfile.STATE_CONNECTING:
 								break;
 							case BluetoothProfile.STATE_CONNECTED:
 								if (Constants.MY_IMEI.equals(Utils
 										.getImei(context))
 										&& Constants.MY_CAR_A2DP_NAME
-												.equals(deviceName))
-								{
+												.equals(deviceName)) {
 									AudioManager mAudioManager = (AudioManager) context
 											.getSystemService(Context.AUDIO_SERVICE);
 									int volume = (int) (mAudioManager
@@ -85,8 +69,7 @@ public class BluetoothReceiver extends BroadcastReceiver {
 								break;
 							case BluetoothProfile.STATE_DISCONNECTED:
 								int state = adapter.getState();
-								if (state == BluetoothAdapter.STATE_ON)
-								{
+								if (state == BluetoothAdapter.STATE_ON) {
 									adapter.disable();
 									Log.d("BluetoothReceiver",
 											"Bluetooth hardware off.");
@@ -94,8 +77,7 @@ public class BluetoothReceiver extends BroadcastReceiver {
 								if (Constants.MY_IMEI.equals(Utils
 										.getImei(context))
 										&& Constants.MY_CAR_A2DP_NAME
-												.equals(deviceName))
-								{
+												.equals(deviceName)) {
 									AudioManager mAudioManager = (AudioManager) context
 											.getSystemService(Context.AUDIO_SERVICE);
 									int volume = (int) (mAudioManager
