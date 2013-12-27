46bcbafad25952559c5b5fd741013e945955b60c
hui.yang <hui.yang@dena.jp>
2013-12-27 15:43:47 +0800

    add crash report support

--- src/com/eric/autowifi/MyApplication.java
+++ src/com/eric/autowifi/MyApplication.java
@@ -1,15 +1,26 @@
 package com.eric.autowifi;
 
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.annotation.ReportsCrashes;
+
 import android.app.Application;
 
+import com.eric.autowifi.utils.CrashSender;
 import com.eric.profile.db.ProfileDB;
 
+@ReportsCrashes(formKey = "", customReportContent = {
+		ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION,
+		ReportField.PHONE_MODEL, ReportField.BRAND, ReportField.STACK_TRACE }, formUri = "http://www.backendofyourchoice.com/reportpath")
 public class MyApplication extends Application {
 	private ProfileDB pdb;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		ACRA.init(this);
+		CrashSender sender = new CrashSender(this);
+		ACRA.getErrorReporter().setReportSender(sender);
 	}
 
 	@Override
